f7dd83cfefb18e41e019651c132647c9ab21c600
phoenixsubash <poudelsubash1234@gmail.com>
2023-08-06 21:06:28 +0545

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ inflate: false }));
+
+app.get("/health", (req, res) => {
+  console.log("req", req);
+  res.write({ check: "Hello World!!!" });
+});
+
+app.listen(process.env.PORT || 3000);
